2e66328033a44782cc5f768c839225697024eec7
heathergibb <hrgibb@gmail.com>
2025-03-06 11:42:52 -0700

    Completed W01 Code: Dynamic Arrays assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,21 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Create an array of type double with a fixed size of 'length'
+        // Given a 'number', this array will store multiples of that number
+        // Assume that length is an integer greater than 0, so don't validate the value of length.
+        double[] result = new double[length];
+        
+        // Use a loop to iterate through each index of the new array
+        // The loop will run for 'length' times, starting at index 0
+        for (int i = 0; i < length; ++i) {
+            // Calculate multiples of the 'number' starting at 1 and ending at the 'length' of the array
+            // Add 1 to the array index (i + 1) to ensure the multiples start from 1 and not 0
+            // and multiply by the given 'number' to get the result for each index
+            result[i] = number * (i + 1);
+        }
+
+        return result; // return the array of multiples
     }
 
     /// <summary>
@@ -29,5 +43,18 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Slice the 'data' list into two separate lists, 
+        // using the total list items minus the 'amount' as the split point
+        int splitPoint = data.Count - amount;
+        // list1 will contain the all the items up to the split point
+        List<int> list1 = data.GetRange(0, splitPoint);
+        // list2 will contain all the items after the split point
+        List<int> list2 = data.GetRange(splitPoint, amount);
+
+        // Save the two lists to the 'data' list, putting list2 first 
+        data.Clear();
+        data.AddRange(list2);
+        data.AddRange(list1);
     }
 }
